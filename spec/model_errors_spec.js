e73bf410e1f0c2afaa66cffa89f1a75828c53b68
Josh Leitzel <joshleitzel@gmail.com>
2011-11-21 13:44:15 -0500

    Brought in files from done/market.

---
+++ spec/model_errors_spec.js
@@ -0,0 +1,119 @@
+var helper = require('../spec_helper'),
+    assert = require('assert'),
+    vows = require('vows'),
+    ModelErrors = require('done').done_model_errors;
+
+var errorText1 = 'You never call me anymore';
+var errorText2 = 'Jellybeans are not an apology';
+
+vows.describe('ModelErrors').addBatch({
+  'new errors object': {
+    topic : new ModelErrors(),
+    'should have no errors' : function (errors) {
+      assert.isTrue(errors.isEmpty());
+    },
+    'should have no error on base' : function (errors) {
+      assert.isNull(errors.on_base());
+    },
+    'should have no error on a field' : function (errors) {
+      assert.isNull(errors.on('field'));
+    },
+  }
+}).addBatch({
+  'with a field error': {
+    topic: function () {
+      var errors = new ModelErrors();
+      errors.add('field', errorText1);
+      this.callback(null, errors);
+    },
+    'should have errors' : function (errors) {
+      assert.isFalse(errors.isEmpty());
+    },
+    'should have exactly one error' : function (errors) {
+      assert.equal(errors.count(), 1);
+    },
+    'should have an error on field' : function (errors) {
+      assert.equal(errors.on('field'), errorText1);
+    },
+    'should have no error on base' : function (errors) {
+      assert.isNull(errors.on_base());
+    },
+    'should have no error on other field' : function (errors) {
+      assert.isNull(errors.on('field2'));
+    },
+    'when cleared' : {
+      topic : function (errors) {
+        errors.clear();
+        this.callback(null, errors);
+      },
+      'should have no errors' : function (errors) {
+        assert.isTrue(errors.isEmpty());
+      }
+    }
+  }
+}).addBatch({
+  'with two field errors': {
+    topic: function () {
+      var errors = new ModelErrors();
+      errors.add('field', errorText1);
+      errors.add('field', errorText2);
+      this.callback(null, errors);
+    },
+    'should have errors' : function (errors) {
+      assert.isFalse(errors.isEmpty());
+    },
+    'should have exactly two errors' : function (errors) {
+      assert.equal(errors.count(), 2);
+    },
+    'should have two errors on field' : function (errors) {
+      assert.equal(errors.on('field').length, 2);
+    },
+    'should have the correct errors on field' : function (errors) {
+      assert.isTrue(_.isEqual(errors.on('field'), [errorText1, errorText2]));
+    },
+    'should have no error on base' : function (errors) {
+      assert.isNull(errors.on_base());
+    },
+    'should have no error on other field' : function (errors) {
+      assert.isNull(errors.on('field2'));
+    },
+    'when cleared' : {
+      topic : function (errors) {
+        errors.clear();
+        this.callback(null, errors);
+      },
+      'should have no errors' : function (errors) {
+        assert.isTrue(errors.isEmpty());
+      }
+    }
+  }
+}).addBatch({
+  'with a base error': {
+    topic: function () {
+      var errors = new ModelErrors();
+      errors.add_to_base(errorText1);
+      this.callback(null, errors);
+    },
+    'should have errors' : function (errors) {
+      assert.isFalse(errors.isEmpty());
+    },
+    'should have exactly one error' : function (errors) {
+      assert.equal(errors.count(), 1);
+    },
+    'should have an error on base' : function (errors) {
+      assert.equal(errors.on_base(), errorText1);
+    },
+    'should have no error on field' : function (errors) {
+      assert.isNull(errors.on('field'));
+    },
+    'when cleared' : {
+      topic : function (errors) {
+        errors.clear();
+        this.callback(null, errors);
+      },
+      'should have no errors' : function (errors) {
+        assert.isTrue(errors.isEmpty());
+      }
+    }
+  }
+}).export(module);
---
+++ spec/model_spec.js
@@ -0,0 +1,229 @@
+// -------------------------------------------------------------------------79
+// Done. Model abstraction spec
+// Copyright (c) 2011 Done. Corporation
+// ---------------------------------------------------------------------------
+var helper = require('../spec_helper'),
+    assert = require('assert'),
+    vows = require('vows'),
+    check = require('validator').check,
+    oo = require('done').oo,
+    DoneModel = require('done').done_model;
+
+var validationText = 'must not be null and should be at least 3 characters';
+
+var SubWidget = DoneModel.extend({}, {
+  Schema : {
+    name : true
+  }
+});
+
+SubWidget.validates('name', function (value, message) {
+    return check(value, message).notNull().len(3, 20);
+  }, validationText);
+
+// DoneModel is designed to be mixed in
+var WidgetBase = DoneModel.extend({}, {
+  Schema : {
+    name : true,
+    additionalField : true,
+    setForError : true,
+    exampleWidget : SubWidget, // single embedded widget
+    examples : [SubWidget],    // array of embedded widgets,
+    dataArray : true
+  }
+});
+
+var Widget = function () {
+  this.doneModelInitialize.apply(this, arguments);
+};
+Widget.mixin = oo.mixin;
+Widget.mixin(WidgetBase);
+
+Widget.validates('name', function (value, message) {
+    return check(value, message).notNull().len(3, 20);
+  }, validationText);
+
+Widget.validates(function (message) {
+    return check(this.get('setForError'), message).isNull();
+  }, 'base error');
+
+
+function create(obj) {
+  return function () {
+    var widget = new Widget(obj);
+    widget.isValid();
+    this.callback(null, widget);
+  };
+}
+
+vows.describe('DoneModel').addBatch({
+// ---------------------------------------------------------------------------
+// Schema behavior
+// ---------------------------------------------------------------------------
+  'widget' : {
+    topic : create({ name : 'myName' }),
+    'can get schema field [name]' : function (err, widget) {
+      assert.equal(widget.get('name'), 'myName');
+    },
+    'exception to get non-schema field [birthdate]' : function (err, widget) {
+      try {
+        widget.get('birthdate');
+      } catch (err) {
+        assert.equal(err.message, 'Schema error: birthdate');
+        return;
+      }
+      assert.ok(false); // Should not get here
+    }
+  },
+  'valid widget with non-schema field' : {
+    topic : create({ name : 'paul', birthdate : 'june' }),
+    'widget should not be valid' : function (err, widget) {
+      assert.isFalse(widget.isValid());
+    },
+    'should have error on birthdate': function (err, widget) {
+      assert.equal(widget.errors.on('birthdate'), 'Schema error');
+    }
+  },
+
+  //
+  // Embedded widget
+  //
+
+  'widget with single embedded widget' : {
+    topic : create({ exampleWidget : { name : 'example' } }),
+    'can get embedded widget' : function (err, widget) {
+      assert.ok(widget.get('exampleWidget'));
+    },
+    'embedded widget has the right type' : function (err, widget) {
+      assert.isTrue(widget.get('exampleWidget') instanceof SubWidget);
+    },
+    'can get embedded widget field' : function (err, widget) {
+      assert.equal(widget.get('exampleWidget').get('name'), 'example');
+    }
+  },
+  'widget with single valid embedded widget' : {
+    topic : create({ name : 'validName', exampleWidget : { } }),
+    'embedded widget should not be valid' : function (err, widget) {
+      assert.isFalse(widget.get('exampleWidget').isValid());
+    },
+    'embedded widget should have error on name': function (err, widget) {
+      var example = widget.get('exampleWidget');
+      example.isValid();
+
+      assert.equal(example.errors.on('name'), validationText);
+    },
+    'widget should not be valid' : function (err, widget) {
+      assert.isFalse(widget.isValid());
+    },
+    'widget should have error on exampleWidget': function (err, widget) {
+      var example = widget.get('exampleWidget');
+      example.isValid();
+
+      assert.deepEqual(widget.errors.on('exampleWidget'), example.errors);
+    }
+  },
+
+  //
+  // Embedded Widget Array
+  //
+
+  'widget with embedded widget array' : {
+    topic : create({ examples : [{ name : 'example1' }, { name : 'example2' }] }),
+    'can get embedded widget array' : function (err, widget) {
+      assert.ok(widget.get('examples'));
+    },
+    'embedded widget array has the right type' : function (err, widget) {
+      assert.isTrue(widget.get('examples') instanceof Array);
+      assert.isTrue(widget.get('examples')[0] instanceof SubWidget);
+    },
+  },
+  'widget with embedded widget array that has an invalid element' : {
+    topic : create({ name : 'validName', examples : [{        }, { name : 'example2' }] }),
+    'widget should not be valid' : function (err, widget) {
+      assert.isFalse(widget.isValid());
+    },
+    'widget should have error on exampleWidget': function (err, widget) {
+      assert.equal(widget.errors.on('examples'), 'Embedded array validation errors');
+    }
+  },
+
+  // Field array
+
+  'widget with array' : {
+    topic : create({ name : 'validName', dataArray : [1, 2, 3] }),
+    'widget should be valid' : function (err, widget) {
+      assert.isTrue(widget.isValid());
+    }
+  }
+
+}).addBatch({
+// ---------------------------------------------------------------------------
+// update_attributes function
+// ---------------------------------------------------------------------------
+
+  'widget with value' : {
+    topic : create({ name : 'myName', additionalField : 'additional' }),
+    'update_attributes schema field' : {
+      topic : function (widget) {
+        return widget.update_attributes({ name : 'newName' });
+      },
+      'will update properly' : function (widget) {
+        assert.equal(widget.get('name'), 'newName');
+      },
+      'does not change other fields' : function (widget) {
+        assert.equal(widget.get('additionalField'), 'additional');
+      }
+    },
+    'update_attributes invalid field' : {
+      topic : function (widget) {
+        return widget.update_attributes({ birthdate : 'June' });
+      },
+      'will fail validation' : function (widget) {
+        assert.isFalse(widget.isValid());
+        assert.ok(widget.errors.on('birthdate'));
+      }
+    },
+  }
+}).addBatch({
+// ---------------------------------------------------------------------------
+// Validation behavior
+// ---------------------------------------------------------------------------
+  'with invalid field in widget': {
+    topic: create({}),
+    'should be invalid': function (widget) {
+      assert.isFalse(widget.isValid());
+    },
+    'should have an error on name': function (widget) {
+      assert.equal(widget.errors.on('name'), validationText);
+    },
+  }
+}).addBatch({
+  'with invalid base record widget': {
+    topic: create({ name : 'Valid Name', setForError : true }),
+    'should be invalid': function (widget) {
+      assert.isFalse(widget.isValid());
+    },
+    'should have an error on base': function (widget) {
+      assert.equal(widget.errors.on_base(), 'base error');
+    },
+    'should not have an error on name': function (widget) {
+      assert.isNull(widget.errors.on('name'));
+    },
+  }
+}).addBatch({
+  'with two invalid params': {
+    topic: create({ name : 'ab', setForError : true }),
+    'should be invalid': function (widget) {
+      assert.isFalse(widget.isValid());
+    },
+    'should have an error on base': function (widget) {
+      assert.equal(widget.errors.on_base(), 'base error');
+    },
+    'should have an error on name': function (widget) {
+      assert.equal(widget.errors.on('name'), validationText);
+    },
+    'should have two errors': function (widget) {
+      assert.equal(widget.errors.count(), 2);
+    },
+  }
+}).export(module);
